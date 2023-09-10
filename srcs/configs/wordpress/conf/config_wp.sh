397e13883b7b87021339707eb1feca53949fef3f
BaBreton <baptiste.br@icloud.com>
2023-09-10 10:58:23 +0200

    Add good architecture but non working project

---
+++ srcs/configs/wordpress/conf/config_wp.sh
@@ -0,0 +1,6 @@
+sleep 10
+wp config create	--allow-root \
+					--dbname=$SQL_DATABASE \
+					--dbuser=$SQL_USER \
+					--dbpass=$SQL_PASSWORD \
+					--dbhost=mariadb:3306 --path='/var/www/wordpress'
